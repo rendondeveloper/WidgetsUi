33adef95669471c60d8a50a91b086645720fe1de
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-05-10 15:26:25 -0600

    modify snackbar

--- lib/widget/snackbar/snackbar_widget.dart
+++ lib/widget/snackbar/snackbar_widget.dart
@@ -7,6 +7,7 @@ void showSnackbarWidget({
   SnackBarBehavior behavior = SnackBarBehavior.fixed,
   Duration duration = const Duration(milliseconds: 3000),
   direction = DismissDirection.up,
+  border = BorderRadius.zero,
   Widget? icon,
 }) {
   ScaffoldMessenger.of(context)
@@ -16,17 +17,16 @@ void showSnackbarWidget({
         dismissDirection: direction,
         duration: duration,
         shape: RoundedRectangleBorder(
-          side: BorderSide(color: color, width: 1.0),
-        ),
+            side: BorderSide(color: color, width: 1.0), borderRadius: border),
         behavior: behavior,
         content: Padding(
           padding: const EdgeInsets.only(right: 24, left: 24),
           child: Row(
             mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              if (icon != null) Center(child: icon),
+              if (icon != null) icon,
               if (icon != null)
                 const SizedBox(
                   width: 8,
