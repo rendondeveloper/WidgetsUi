d1b1372223fec1804fdf68c84fd9c7c5d90a8f44
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-05-17 18:15:14 -0600

    change loader

--- lib/widget/loader/loader.dart
+++ lib/widget/loader/loader.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class LoaderWidget extends StatelessWidget {
   final Widget? message;
-  const LoaderWidget({required this.message, Key? key}) : super(key: key);
+  const LoaderWidget({this.message, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
