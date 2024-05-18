283a0096077630bff080b418e8a05d28f1f3d294
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-05-17 18:21:50 -0600

    change loader

--- lib/widget/loader/loader.dart
+++ lib/widget/loader/loader.dart
@@ -15,7 +15,7 @@ class LoaderWidget extends StatelessWidget {
         margin: const EdgeInsets.symmetric(horizontal: 20.0),
         padding: const EdgeInsets.symmetric(horizontal: 10.0),
         height: 60,
-        color: Colors.white,
+        color: message != null ? Colors.white : Colors.transparent,
         child: Row(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -40,38 +40,4 @@ class LoaderWidget extends StatelessWidget {
       ),
     );
   }
-}
-
-Widget loaderWidget({required context, String? text}) {
-  return Container(
-    color: Colors.black38,
-    alignment: Alignment.center,
-    child: Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20.0),
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      height: 60,
-      color: Colors.white,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Platform.isAndroid
-              ? Container(
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                  child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                          AppBarTheme.of(context).backgroundColor ??
-                              Theme.of(context).colorScheme.secondary),
-                      backgroundColor: Colors.white),
-                )
-              : const CupertinoActivityIndicator(),
-          Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Text(text ?? "",
-                  style: Theme.of(context).textTheme.titleSmall)),
-        ],
-      ),
-    ),
-  );
 }
