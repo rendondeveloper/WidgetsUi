3c9b4d595b3d3d0c756b500b6e4bb8046a2e88e9
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-05-25 07:00:08 -0600

    add loader

--- lib/widget/image/image_network_widget.dart
+++ lib/widget/image/image_network_widget.dart
@@ -4,19 +4,27 @@ class ImageNetWotkWidget extends StatelessWidget {
   final String _url;
   final BoxFit _fit;
   final Widget _icon;
+  final Widget? _loader;
 
   const ImageNetWotkWidget(this._url,
-      {BoxFit fit = BoxFit.fill,
+      {Widget? loader,
+      BoxFit fit = BoxFit.fill,
       Widget icon = const Icon(Icons.image_not_supported_outlined),
       super.key})
       : _fit = fit,
-        _icon = icon;
+        _icon = icon,
+        _loader = loader;
 
   @override
   Widget build(BuildContext context) {
     return Image.network(
       _url,
       fit: _fit,
+      loadingBuilder: (BuildContext context, Widget child,
+          ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) return child;
+        return _loader ?? const CircularProgressIndicator.adaptive();
+      },
       errorBuilder: (context, error, stackTrace) {
         return _icon;
       },
