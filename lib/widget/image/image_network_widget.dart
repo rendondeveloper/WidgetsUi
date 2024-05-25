6b04773599a507004ab6eba15f109f0a8e48f750
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-05-25 06:54:05 -0600

    change funtion to stalesswidget

--- lib/widget/image/image_network_widget.dart
+++ lib/widget/image/image_network_widget.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
-Widget imageNetWotkWidget(String url,
-    {BoxFit fit = BoxFit.fill,
-    Widget icon = const Icon(Icons.image_not_supported_outlined)}) {
-  return Image.network(
-    url,
-    fit: fit,
-    errorBuilder: (context, error, stackTrace) {
-      return icon;
-    },
-  );
+class ImageNetWotkWidget extends StatelessWidget {
+  final String _url;
+  final BoxFit _fit;
+  final Widget _icon;
+
+  const ImageNetWotkWidget(this._url,
+      {BoxFit fit = BoxFit.fill,
+      Widget icon = const Icon(Icons.image_not_supported_outlined),
+      super.key})
+      : _fit = fit,
+        _icon = icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      _url,
+      fit: _fit,
+      errorBuilder: (context, error, stackTrace) {
+        return _icon;
+      },
+    );
+  }
 }
