e6f8e60798c5317b28093aaa73b2a7a2a239939e
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-06-15 12:29:38 -0600

    add label_title_widget

---
+++ lib/widget/label/label_title_subtile_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LabelTitleSubtitleWidget extends StatelessWidget {
+  final String title;
+  final String subtTitle;
+  final TextStyle? styleLabel;
+  final int maxLinesLabel;
+  final TextStyle? styleData;
+  final int maxLinesData;
+  final CrossAxisAlignment crossAxisAlignment;
+
+  const LabelTitleSubtitleWidget(
+      {required this.title,
+      required this.subtTitle,
+      this.crossAxisAlignment = CrossAxisAlignment.start,
+      this.maxLinesLabel = 3,
+      this.maxLinesData = 3,
+      this.styleLabel,
+      this.styleData,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(title,
+            softWrap: true,
+            maxLines: maxLinesLabel,
+            style: styleLabel ??
+                Theme.of(context)
+                    .textTheme
+                    .labelSmall
+                    ?.copyWith(fontWeight: FontWeight.normal)),
+        const SizedBox(height: 8.0),
+        Text(subtTitle,
+            softWrap: true,
+            maxLines: maxLinesData,
+            overflow: TextOverflow.ellipsis,
+            style: styleData ?? Theme.of(context).textTheme.labelSmall)
+      ],
+    );
+  }
+}
