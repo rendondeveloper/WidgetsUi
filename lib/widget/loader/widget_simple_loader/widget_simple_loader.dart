84eeb24bc1eea4deaeef9d37450400332d255f6c
luis rendon <luis.rendon@aplazo.mx>
2024-10-30 18:29:26 -0600

    🎨 Adjust the visibility widget

--- lib/widget/loader/widget_simple_loader/widget_simple_loader.dart
+++ lib/widget/loader/widget_simple_loader/widget_simple_loader.dart
@@ -16,31 +16,32 @@ class WidgetSimpleLoader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      color: props?.background ?? Colors.black26,
-      alignment: Alignment.center,
+    return Visibility(
+      visible: showLoader,
       child: Container(
-        decoration: BoxDecoration(
-          color: props?.content != null
-              ? props?.backgroundContainer ?? Colors.white
-              : props?.backgroundIndicator ??
-                  Theme.of(context)
-                      .progressIndicatorTheme
-                      .refreshBackgroundColor,
-          borderRadius:
-              props?.borderRadius ?? BorderRadius.circular(conerMedium),
-        ),
-        margin: const EdgeInsets.symmetric(horizontal: 20.0),
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        height: props?.content == null ? 60 : 100,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (showLoader)
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: props?.background ?? Colors.black26,
+        alignment: Alignment.center,
+        child: Container(
+          decoration: BoxDecoration(
+            color: props?.content != null
+                ? props?.backgroundContainer ?? Colors.white
+                : props?.backgroundIndicator ??
+                    Theme.of(context)
+                        .progressIndicatorTheme
+                        .refreshBackgroundColor,
+            borderRadius:
+                props?.borderRadius ?? BorderRadius.circular(conerMedium),
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          height: props?.content == null ? 60 : 100,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
               Platform.isAndroid
                   ? Container(
                       decoration: const BoxDecoration(
@@ -51,17 +52,22 @@ class WidgetSimpleLoader extends StatelessWidget {
                             Theme.of(context).progressIndicatorTheme.color,
                       ),
                     )
-                  : const CupertinoActivityIndicator(),
-            if (props?.content != null) const SizedBox(width: separationLarge),
-            if (props?.content != null)
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  props?.content ?? Container(),
-                ],
-              ),
-          ],
+                  : CupertinoActivityIndicator(
+                      color: props?.color ??
+                          Theme.of(context).progressIndicatorTheme.color,
+                    ),
+              if (props?.content != null)
+                const SizedBox(width: separationLarge),
+              if (props?.content != null)
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    props?.content ?? Container(),
+                  ],
+                ),
+            ],
+          ),
         ),
       ),
     );
